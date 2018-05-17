254f6ea588386ea9ba447ea69abec458df513991
Guillermo Fernández <fguille94@outlook.com>
2018-05-17 17:16:23 -0400

    ff

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 1337;
 const mysql = require('mysql');
+let bodyParser = require('body-parser');
 
 let con = mysql.createPool({
     connectionLimit : 10, // default = 10
@@ -11,11 +13,33 @@ let con = mysql.createPool({
     database        : 'ayyqr'
 });
 
+
+// create application/json parser
+let jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// POST /login gets urlencoded bodies
+app.post('/login', urlencodedParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    console.log('welcome, ' + req.body.username);
+    res.send('welcome, ' + req.body.username);
+});
+
+// POST /api/users gets JSON bodies
+app.post('/api/users2', jsonParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400)
+    // create user in req.body
+});
+
 app.post('/api/users/signup', function (req, res) {
     con.getConnection(function (err, conx) {
-        conx.query("INSERT INTO users (username) VALUES ('a')", function (err, rows) {
+        conx.query("SELECT * FROM users WHERE fname=2", function (err, rows) {
+        // conx.query("INSERT INTO users (username) VALUES ('a')", function (err, rows) {
             conx.release();
             if (err) throw err;
+            console.log(req.body.username);
             console.log('Field count:', rows.fieldCount);
             console.log('Affected rows:', rows.affectedRows);
             console.log('Changed rows:', rows.changedRows);
